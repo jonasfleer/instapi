0b095e07ac4b899649c159f149d0b58747d262c1
jonasfleer <jonas.fleer@gmx.net>
2013-08-27 01:28:35 +0000

    First working version - printing still missing.

--- ui/htdocs/instapi_pics.php
+++ ui/htdocs/instapi_pics.php
@@ -1,8 +1,10 @@
 <?php
 require_once("globals.php");
+require_once(INSTAPI_DIR . 'libs/funcs.lib.php');
+
 $id = $_GET['id'];
 $type = $_GET['type'];
-$geometry = empty($_GET['geometry']) ? '130' : $_GET['geometry'];
+$geometry = empty($_GET['geometry']) ? THUMB_WIDTH : $_GET['geometry'];
 
 if ($type=="thumb") {
     $im = file_get_contents(THUMB_DIR . '/' . computeThumbfileName($id, $geometry) . '.jpg'); 
--- ui/smarty/libs/picsession_setup.php
+++ ui/smarty/libs/picsession_setup.php
@@ -1,7 +1,7 @@
 <?php
 
 require(INSTAPI_DIR . 'libs/picsession.lib.php');
-require('Smarty/Smarty.class.php');
+require('smarty3/Smarty.class.php');
 
 // smarty configuration
 class PicSession_Smarty extends Smarty {
@@ -14,6 +14,16 @@ class PicSession_Smarty extends Smarty {
       if (!is_dir(TEMP_DIR)) {
           if(!mkdir(TEMP_DIR)) { 
             error_log("can't create temp path at: " . TEMP_DIR); 
+          }
+      }
+      if (!is_dir(THUMB_DIR)) {
+          if(!mkdir(THUMB_DIR)) { 
+            error_log("can't create temp path at: " . THUMB_DIR); 
+          }
+      }
+      if (!is_dir(PRINT_MASTER_DIR)) {
+          if(!mkdir(PRINT_MASTER_DIR)) { 
+            error_log("can't create temp path at: " . PRINT_MASTER_DIR); 
           }
       }
     }
